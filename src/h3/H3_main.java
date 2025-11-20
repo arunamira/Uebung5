5f5d5661c8db03efcd217c68e523c68f3b75c08a
Aruna Anhuf <a.anhuf@mail.com>
2025-11-20 17:46:06 +0100

    H3

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -2,6 +2,16 @@ package h3;
 
 public class H3_main { public static void main(String[] args) {
 	
+	int[][]einheiten = {{50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1}, {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+	int input = 6279;
+	
+	for (int i = 0; i<15; i++) {
+		einheiten[1][i] = input / einheiten[0][i];
+		input = input - (einheiten[1][i]*einheiten[0][i]);
+		System.out.println(einheiten[1][i] + " " + einheiten[0][i]);
+	}
+	
 }
 
 }
+  

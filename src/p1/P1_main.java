d7f469535c173b0c83283eef615c0b58aa3307e1
Aruna Anhuf <a.anhuf@mail.com>
2025-11-20 14:31:46 +0100

    P1

---
+++ src/p1/P1_main.java
@@ -0,0 +1,25 @@
+package p1;
+
+public class P1_main {
+	public static void main(String[] args) {
+		int x = 0;
+		while ( x <= 10) {
+			x = x+1;
+			System.out.println(x);
+		}
+		
+		
+		int y = 0;
+		do { 
+			y = y+1;
+			System.out.println(y);
+		} while (y<=10);
+		
+		
+		for (int z = 1; z <= 11; z++) {
+			System.out.println(z);
+		}
+		
+	}
+
+}

d00ddc028b9e27774421647afee1837134b89bf2
Aruna Anhuf <a.anhuf@mail.com>
2025-11-20 17:12:37 +0100

    H2

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -3,6 +3,28 @@ package h2;
 public class H2_main {
 	public static void main(String[] args) {
 		
+		int n = 469956, digits = 6;
+		int []a= {0,0,0,0,0,0,0,0,0};
+		
+		String s = String.valueOf(n);
+		digits = s.length();
+	
+		System.out.print("Anzahl Digits: " + digits);
+		System.out.println();
+	
+	
+		
+		for (int i = 8; i > 8 - digits; i--) {
+			a[i] = n % 10;
+			n = n/10;
+		}
+		System.out.println("Array a: ");
+		for (int x = 0; x<9; x++) {
+			System.out.print(a[x] + " ");
+			
+			
+		}
+		}
 	}
 
-}
+
